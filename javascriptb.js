39a9213e2a62de02c415a85ef89f52694ef10df2
Fyyrerde4xhsj <ankurboro236@gmail.com>
2026-02-02 16:43:33 +0530

    Delete javascriptb.js
    You

--- javascriptb.js
+++
@@ -1,8 +0,0 @@
-function sendWithdrawalEmail(upiId, amount) {
-    const email = "ankurboro236@gmail.com";
-    const subject = "Withdrawal Request";
-    const body = `User with UPI ID: ${upiId} has requested withdrawal of ₹${amount.toFixed(2)}.`;
-    
-    // Open default email client
-    window.open(`mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`);
-}
